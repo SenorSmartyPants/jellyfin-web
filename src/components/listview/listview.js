0979cdb62c37b98fcfd61c958bd9fbf7d831e58b
SenorSmartyPants <senorsmartypants@gmail.com>
2022-11-21 18:11:20 -0600

    Display whole channel icon and new/repeat/live/premiere flags
    - Make timer list visually identical when displayed on Series Timer page, or Series page.
    - Display full channel icon (background-size: contain)
    - Display New/Repeat/etc in timer list. Respects user guide settings for which flags to display
    - Add margin before timerIndicator if mediainfo is present
    - Add option to display officialRating display in mediainfo
    - Use ids instead of CSS class for seriesTimerSchedule*

--- src/components/listview/listview.js
+++ src/components/listview/listview.js
@@ -261,6 +261,10 @@ import ServerConnections from '../ServerConnections';
                 const imgUrl = options.imageSource === 'channel' ? getChannelImageUrl(item, downloadWidth) : getImageUrl(item, downloadWidth);
                 let imageClass = isLargeStyle ? 'listItemImage listItemImage-large' : 'listItemImage';
 
+                if (options.imageSource === 'channel') {
+                    imageClass += ' listItemImage-channel';
+                }
+
                 if (isLargeStyle && layoutManager.tv) {
                     imageClass += ' listItemImage-large-tv';
                 }
@@ -433,11 +437,12 @@ import ServerConnections from '../ServerConnections';
                     html += '<div class="secondary listItemMediaInfo">';
                     html += mediaInfo.getPrimaryMediaInfoHtml(item, {
 
-                        year: false,
-                        container: false,
-                        episodeTitle: false,
-                        criticRating: false,
-                        endsAt: false
+                    year: false,
+                    container: false,
+                    episodeTitle: false,
+                    criticRating: false,
+                    officialRating: false,
+                    endsAt: false
 
                     });
                     html += '</div>';
